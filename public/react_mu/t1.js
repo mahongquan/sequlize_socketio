72949cf0df3abd21f8b6ce6a4c9f681717fd545c
mahongquan <mahongquan@sina.com>
2017-09-28 09:48:29 +0800

    mu ok

---
+++ public/react_mu/t1.js
@@ -0,0 +1,23 @@
+// var p = new Promise(function(resolve, reject){
+//     //做一些异步操作
+//     setTimeout(function(){
+//         console.log('执行完成');
+//         resolve('随便什么数据');
+//     }, 2000);
+// });
+function runAsync(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('执行完成');
+            resolve('随便什么数据');
+        }, 2000);
+    });
+    return p;            
+}
+// runAsync().then(function(data){
+//     console.log(data);
+//     //后面可以用传过来的数据做些其他操作
+//     //......
+// });
+console.log(new Date())
